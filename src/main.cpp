36899e64c9c4f789ec8c9ac795baabebbb5ef1f2
Dabaridu <david.turk04@gmail.com>
2025-10-13 17:27:04 +0200

    Added kinematics limit to axes 2 and 3 setup controll homing sequence

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,14 @@ Servo servo3;
 Servo servo4;
 Servo servo5;
 
+int servoPos[6];   // Array to store servo positions
+int potValues[6];
+
+const int potRead[] = {A0, A1, A2, A3, A4, A5};
+int homepos[6] = {90, 90, 0, 90, 90, 90};
+int BOT_LIMIT[6] = {0, 0, 0, 0, 90, 0};
+int TOP_LIMIT[6] = {180, 180, 120, 120, 150, 180};
+
 void setup() {
   Serial.begin(9600);
   servo0.attach(2);
@@ -24,51 +32,70 @@ void setup() {
   pinMode(A1, INPUT);
   pinMode(A0, INPUT);
 
-}
+  while (true){
+    bool check[6] = {false, false, false, false, false, false};
 
-void readAnalog(){
-  int pot0 = analogRead(A0);
-  int pot1 = analogRead(A1);
-  int pot2 = analogRead(A2);
-  int pot3 = analogRead(A3);
-  int pot4 = analogRead(A4);
-  int pot5 = analogRead(A5);
+    Serial.print("Set pot 90°: ");
 
-  Serial.print("pot0: ");
-  Serial.print(pot0);
-  Serial.print(" pot1: ");
-  Serial.print(pot1);
-  Serial.print(" pot2: ");
-  Serial.print(pot2);
-  Serial.print(" pot3: ");
-  Serial.print(pot3);
-  Serial.print(" pot4: ");
-  Serial.print(pot4);
-  Serial.print(" pot5: ");
-  Serial.println(pot5);
+    for (int i = 0; i < 6; i++) {
+      int potValue = map(analogRead(potRead[i]),0,1023,0,180);  // Read once and store
+      
+      Serial.print("Pot: ");
+      Serial.print(homepos[i]);  // Add index number
+      Serial.print("=");
+      Serial.print(potValue);  // Add line break
+      Serial.print("  ");
+      
+      if (potValue > homepos[i]-5 && potValue < homepos[i]+5) {
+          check[i] = true;
+          //Serial.println("Condition met!");
+      }else{
+          //Serial.println("Condition not met.");
+          check[i] = false;
+      }
+    }
+    Serial.print("\n");
+    
+    if (check[0] && check[1] && check[2] && check[3] && check[4] && check[5]){
+      break;
+    }
+  }  
 }
 
 void moveServos() {
   // Map potentiometer readings (0-1023) to servo angles (0-180)
-  int servoPos0 = map(analogRead(A0), 0, 1023, 0, 360);
-  int servoPos1 = map(analogRead(A1), 0, 1023, 0, 360);
-  int servoPos2 = map(analogRead(A2), 0, 1023, 0, 360);
-  int servoPos3 = map(analogRead(A3), 0, 1023, 0, 360);
-  int servoPos4 = map(analogRead(A4), 0, 1023, 0, 360);
-  int servoPos5 = map(analogRead(A5), 0, 1023, 0, 360);
+  for (int i = 0; i < 6; i++) {
+    servoPos[i] = map(analogRead(potRead[i]), 0, 1023, BOT_LIMIT[i], TOP_LIMIT[i]);
+    
+    Serial.print(servoPos[i]); Serial.print(" ");
+    if (i == 5) Serial.println();
+  }
+
+  //CHECK SERVO COLISIONS
+  int L3 = 180-servoPos[3]-90; //L3 = 90 - servo3
+  int L2 = servoPos[2]+50;     //L2 = servo2 - 90
+  int Lim = L2-L3;
+  // Serial.print("Lim: "); Serial.print(Lim);
+  // Serial.print(", L2 = "); Serial.print(L2);
+  // Serial.print(", L3 = "); Serial.println(L3);
   
-  // Write positions to servos
-  servo0.write(servoPos0);
-  servo1.write(servoPos1);
-  servo2.write(servoPos2);
-  servo3.write(servoPos3);
-  servo4.write(servoPos4);
-  servo5.write(servoPos5);
+  // Write positions to servos using array
+  servo0.write(servoPos[0]);
+  servo1.write(servoPos[1]);
+  
+  servo2.write(servoPos[2]); //DEF 0, 0-120
+  if(Lim<23){
+    servo3.write(113-L2); //DEF 90, 0-120
+  }else{
+    servo3.write(servoPos[3]); //DEF 90, 0-120
+  }
+  servo4.write(servoPos[4]); //DEF 90, 90-140
+  servo5.write(servoPos[5]); //DEF 90, 0-180
+
 }
 
 
 void loop() {
-  readAnalog();
   moveServos();
   delay(20); // Delay before next reading
 }
